218c44b15c9577dbe1e719fd7de5057a2fca1339
Daniel <alfacedaniel12@gmail.com>
2021-12-03 20:46:53 -0300

    carrinho

---
+++ carrinho.js
@@ -0,0 +1,30 @@
+var cart = [];
+
+
+function pegadados(produto, preco, imagem) {
+    cart.push({
+        produto,
+        preco,
+        imagem
+    });
+    console.log(cart);
+    carrinho()
+}
+
+function carrinho() {
+    let lista = document.getElementById("carrinho-item")
+    for (item of cart) {
+        let li = document.createElement("li");
+        let image = document.createElement("img");
+        image.src = item.imagem;
+        li.appendChild(image);
+        let prod = document.createElement("span");
+        prod.innerHTML = item.produto
+        prod.classList.add("produtoz");
+        let cost = document.createElement("span");
+        cost.innerHTML = item.preco
+        li.appendChild(prod);
+        li.appendChild(cost);
+        lista.appendChild(li);
+    }
+}
