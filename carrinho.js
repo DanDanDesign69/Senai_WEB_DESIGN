ffd5a8be7bf17e51c7e583ceef635c7cfb6dd4a6
Daniel <alfacedaniel12@gmail.com>
2021-12-06 21:24:30 -0300

    carrinho fixed

--- carrinho.js
+++ carrinho.js
@@ -13,13 +13,15 @@ function pegadados(produto, preco, imagem) {
 
 function carrinho() {
     let lista = document.getElementById("carrinho-item")
+    lista.innerHTML = "";
     for (item of cart) {
         let li = document.createElement("li");
         let image = document.createElement("img");
+        image.classList.add("produteros");
         image.src = item.imagem;
         li.appendChild(image);
         let prod = document.createElement("span");
-        prod.innerHTML = item.produto
+        prod.innerHTML = item.produto;
         prod.classList.add("produtoz");
         let cost = document.createElement("span");
         cost.innerHTML = item.preco
